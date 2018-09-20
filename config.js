80794c337c270b0646064cb7e3d1356d97b77722
Xavier Fernandez <xavier@crservers.com>
2018-09-20 12:16:21 -0600

    Initial commit
    Add API monitor

---
+++ config.js
@@ -0,0 +1,37 @@
+/**********************************************
+*
+* CryptoLions.io
+* https://github.com/CryptoLions/EOS-Testnet-monitor
+*
+***********************************************/
+var _reqInterval = 1000;
+
+var blockProducerList = [
+  {
+    "bp_name": "Load-Balanced",
+    "organisation": "EOS Costa Rica",
+    "location": "San Jose, Costa Rica",
+    "node_addr": "api.eosio.cr",
+    "port_http": "443",
+    "port_ssl": "443",
+    "port_p2p": "1976"
+  },
+  {
+    "bp_name": "FullNode1",
+    "organisation": "EOS Costa Rica",
+    "location": "San Jose, Costa Rica",
+    "node_addr": "costarica1.eosio.cr",
+    "port_http": "443",
+    "port_ssl": "443",
+    "port_p2p": "1976"
+  },
+  {
+    "bp_name": "FullNode2",
+    "organisation": "EOS Costa Rica",
+    "location": "San Jose, Costa Rica",
+    "node_addr": "costarica2.eosio.cr",
+    "port_http": "443",
+    "port_ssl": "443",
+    "port_p2p": "5418"
+  },
+]
